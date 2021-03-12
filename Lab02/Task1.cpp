e6726a6c983d0bbfbaedaecde6ef36b6484b43d4
DeathNet123 <arslan.computer2002@gmail.com>
2021-03-12 17:42:45 +0500

    Task1.cpp added and done

--- Lab02/Task1.cpp
+++ Lab02/Task1.cpp
@@ -1,31 +1,39 @@
-#include <cmath>
-#include <cstdio>
+#include<iostream>
 #include <vector>
-#include <iostream>
+#include <numeric>
 #include <algorithm>
+#include <numeric>
 using namespace std;
 
-int fine_division(int index, int sum, int size)
+int fine_division(int index, int size, vector<int> difference, vector<int> array)
 {
+    int temper;
     //base case..
     if(index == size)
     {
-        
+        int min_index = min_element(difference.begin(), difference.end()) - difference.begin();
+        cout<<min_index + 1;
+        return min_index;
     }
+    int sum = accumulate(array.begin() + index + 1, array.end(), 0);
+    int sum2 = accumulate(array.begin(), array.begin() + index + 1, 0);
+    int minus = abs(sum2 - sum);
+    difference.push_back(minus);
+    fine_division(++index, size, difference, array);
+    return 0;
 }
-
 
 int main(void) 
 {
     int size, temp;
-    vectore <int> array;
+    vector <int> array;
+    vector<int> difference;
     cin>>size;
     for(int idx = 0; idx < size; idx++)
     {
         cin>>temp;
         array.push_back(temp);
     }
-    
-    
+    fine_division(0, size, difference, array);
     return 0;
 }
